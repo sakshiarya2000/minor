b60ed6fc2cad9ea8f86c7f7ee66ec4fd9d07892b
sakshiarya2000 <64597941+sakshiarya2000@users.noreply.github.com>
2021-04-26 13:51:12 +0530

    Update Dao.java

--- Dao.java
+++ Dao.java
@@ -2,6 +2,7 @@
 package Dao;
 
 import Connection.MyCon;
+import Model.Comp;
 import Model.Criminals;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -44,12 +45,12 @@ public int insertDetails(Criminals C) throws SQLException
          ps.setString(3, C.getPass());
          ps.setString(4, C.getFname());
          ps.setString(5, C.getLname());
-         ps.setString(6, C.getEmail());
-         ps.setInt(7, C.getMobile());
-         ps.setString(8, C.getAdr());
-         ps.setString(9, C.getcPass());
-
+         ps.setString(6, C.getAdr());
+         ps.setString(7, C.getCpass());
+         ps.setString(8, C.getEmail());
+         ps.setString(9, C.getMobile());
          int n=0; 
+         System.out.println("insert query : "+ps.toString());
          n=ps.executeUpdate();
          return(n);   
         
@@ -59,7 +60,7 @@ public boolean checkLogin(String user,String pass) throws SQLException
     {
          String sql;
         Connection con=null;
-        sql="select * from signin where user=? and pass=?";
+        sql="select * from signup where uname=? and pass=?";
         PreparedStatement ps=null;
         con=MyCon.getConnection();
         System.err.println("connection created username "+user);
@@ -92,12 +93,12 @@ public boolean checkLogin(String user,String pass) throws SQLException
             {
                 if(rs.next())
                 {
-                    b.setUname(rs.getString(1));
-                    b.setFname(rs.getString(2));
-                    b.setLname(rs.getString(3));
-                    b.setEmail(rs.getString(4));
-                    b.setAdr(rs.getString(5));
-                    b.setMobile(rs.getInt(6));
+                    b.setUname(rs.getString(2));
+                    b.setFname(rs.getString(4));
+                    b.setLname(rs.getString(5));
+                    b.setEmail(rs.getString(8));
+                    b.setAdr(rs.getString(6));
+                    b.setMobile(rs.getString(9));
                 }
                 else
                 {
@@ -108,5 +109,53 @@ public boolean checkLogin(String user,String pass) throws SQLException
      return b;
         
         }
+ 
+ 
+ public int insertComplaint(Comp C) throws SQLException
+    {
+        String sql;
+        Connection con=null;
+       
+        sql="insert into complaint values(?,?,?,?,?,?,?,?,?)";
+        PreparedStatement ps=null;
+         con=MyCon.getConnection();
+         ps=con.prepareStatement(sql);
+         ps.setString(1, C.getName());
+         ps.setString(2, C.getCompid());
+         ps.setString(3, C.getComptype());
+         ps.setString(4, C.getNature());
+         ps.setString(5, C.getEmail());
+         ps.setString(6, C.getDate());
+         ps.setString(7, C.getMobile());
+         ps.setString(8, C.getAdr());
+         ps.setString(9, C.getAadhar());
+         int n=0; 
+         
+         n=ps.executeUpdate();
+         return(n);   
+        
+      }
+ 
+ 
+ 
+ public boolean checkLogin2(String user,String pass) throws SQLException
+    {
+         String sql;
+        Connection con=null;
+        sql="select * from admin where user=? and pass=?";
+        PreparedStatement ps=null;
+        con=MyCon.getConnection();
+        System.err.println("connection created username "+user);
+        ps=con.prepareStatement(sql);
+        System.err.println("connection created "+sql);
+        ps.setString(1, user);
+        ps.setString(2, pass);
+        ResultSet rs=null;
+        rs=ps.executeQuery();
+        if(rs.next())
+          return true;
+        
+     return false;
+    }
     
 }
